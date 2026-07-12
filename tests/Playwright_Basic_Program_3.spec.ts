05afc965cfdf79a4cfd43d79c3ed89f3d8c37c86
sreshma-shaik <sreshu0786@gmail.com>
2026-07-12 16:14:34 +0530

    Add Playwright basic program 3

---
+++ tests/Playwright_Basic_Program_3.spec.ts
@@ -0,0 +1,10 @@
+import {test} from '@playwright/test'
+
+test("RBG demo", async ({ page }) =>  console.log('Playwright Demo'))
+
+
+//arrow function it is a parameterized - mainly 2 types those are 1)string 2)function type;
+
+//sprint level activity(dry run if it is stable and added into regression suite) manual tester/functional tester   
+
+//Regression level activity - excuetion all the cases giving the all cases - automation fails after testing again anad again then we will ask to developer 
